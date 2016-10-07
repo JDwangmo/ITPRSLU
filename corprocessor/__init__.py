6266643b5e74a88954e77f9ec11f5a3fc48abe06
jdwang <383287471@qq.com>
2016-10-07 10:33:57 +0800

    update

---
+++ corprocessor/__init__.py
@@ -0,0 +1,8 @@
+# encoding=utf8
+"""
+    Author:  'jdwang'
+    Date:    'create date: 2016-10-06'; 'last updated date: 2016-10-06'
+    Email:   '383287471@qq.com'
+    Describe:
+"""
+from __future__ import print_function
